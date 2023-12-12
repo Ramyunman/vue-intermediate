fa98cf7718a498c3427ffe581d1d6e3fb74b06ce
Ramyunman <prettyshield@naver.com>
2023-12-12 17:50:50 +0900

    231212 vuex ing

--- vue-todo/src/main.js
+++ vue-todo/src/main.js
@@ -1,10 +1,12 @@
 // main.js
 import { createApp } from 'vue'
 import App from './App.vue'
-import store from './store/store' // 수정된 store 파일 경로
+import store from './store/store'
 
 const app = createApp(App)
 
-app.use(store) // Vuex 스토어를 Vue 앱에 플러그인으로 추가합니다.
+// 데이터 로딩 액션 호출
+store.dispatch('fetchTodoItems')
 
+app.use(store)
 app.mount('#app')
--- vue-todo/src/store/store.js
+++ vue-todo/src/store/store.js
@@ -3,8 +3,30 @@ import { createStore } from 'vuex'
 
 const store = createStore({
   state: {
-    headerText: 'TODO it!'
+    todoItems: []
+  },
+  mutations: {
+    setTodoItems(state, items) {
+      state.todoItems = items;
+    }
+  },
+  actions: {
+    fetchTodoItems(context) {
+      if (localStorage.length > 0) {
+        const items = [];
+        for (let i = 0; i < localStorage.length; i++) {
+          if(localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+            items.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+          }
+        }
+        context.commit('setTodoItems', items);
+      }
+    }
+  },
+  getters: {
+    // ...getters if needed
   }
 });
 
 export default store;
+
