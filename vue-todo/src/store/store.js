ac015b85812ae9d3407f973bcc5ff6159ca2b3b9
Ramyunman <prettyshield@naver.com>
2023-12-12 18:00:08 +0900

    231212 vuex ing

--- vue-todo/src/store/store.js
+++ vue-todo/src/store/store.js
@@ -1,32 +1,38 @@
 // store.js
 import { createStore } from 'vuex'
 
+const storage = {
+  fetch() {
+    const arr = [];
+    if (localStorage.length > 0) {
+      for (let i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+          arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+        }
+      }
+    }
+    return arr;
+  },
+};
+
 const store = createStore({
   state: {
-    todoItems: []
+    todoItems: [], // 초기에 빈 배열로 설정
   },
   mutations: {
     setTodoItems(state, items) {
       state.todoItems = items;
-    }
+    },
   },
   actions: {
     fetchTodoItems(context) {
-      if (localStorage.length > 0) {
-        const items = [];
-        for (let i = 0; i < localStorage.length; i++) {
-          if(localStorage.key(i) !== 'loglevel:webpack-dev-server') {
-            items.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-          }
-        }
-        context.commit('setTodoItems', items);
-      }
-    }
+      const items = storage.fetch(); // 로컬 스토리지에서 데이터를 가져옴
+      context.commit('setTodoItems', items); // 상태 업데이트
+    },
   },
   getters: {
-    // ...getters if needed
-  }
+    // 필요한 경우 추가적인 게터 정의
+  },
 });
 
 export default store;
-
